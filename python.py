d45e90bbdff6c65651cca555980c39e28672a103
krastega0 <krastegar0@gmail.com>
2025-01-23 18:23:50 +0100

    updated readme instructions to generate requirements.dev and requirements.txt

---
+++ python.py
@@ -0,0 +1,7 @@
+import pkg_resources
+
+try:
+    setuptools_version = pkg_resources.get_distribution('setuptools').version
+    print(f"Setuptools version: {setuptools_version}")
+except pkg_resources.DistributionNotFound:
+    print("Setuptools is not installed.")
